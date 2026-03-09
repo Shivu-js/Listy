7c9bd3f16e16fd9ba3edd25636bb27e80a5d4374
Shivu-js <sushil912031@gmail.com>
2026-03-09 10:21:40 +0530

    Add Firebase configuration to service worker

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -2,7 +2,12 @@ importScripts("https://www.gstatic.com/firebasejs/12.10.0/firebase-app-compat.js
 importScripts("https://www.gstatic.com/firebasejs/12.10.0/firebase-messaging-compat.js");
 
 firebase.initializeApp({
-  messagingSenderId: "909038043802"
+  apiKey: "AIzaSyALtFqrxD2TLrQXNvOyJRtsfvi3bbKiCnY",
+  authDomain: "listy-pwa.firebaseapp.com",
+  projectId: "listy-pwa",
+  storageBucket: "listy-pwa.firebasestorage.app",
+  messagingSenderId: "909038043802",
+  appId: "1:909038043802:web:9277e0fa6dc63ec6735523"
 });
 
 const messaging = firebase.messaging();
